01f49a24badd72d0dd44bce2802153822392f959
GigaBuny <130915003+GigaBuny@users.noreply.github.com>
2024-07-06 15:51:40 +0300

    Update yeni 1.lua

--- yeni 1.lua
+++ yeni 1.lua
@@ -14,7 +14,7 @@ local Window = Rayfield:CreateWindow({
       Invite = "HCjknp8G", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
-   KeySystem = True, -- Set this to true to use our key system
+   KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
       Title = "Agalar Hub",
       Subtitle = "Key System",
